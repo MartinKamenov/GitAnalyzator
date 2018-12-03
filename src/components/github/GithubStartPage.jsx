883fd3d509deb1b0cb6e99fc04de4406cfd087bc
MartinKamenov <martinkamenov10@gmail.com>
2018-12-03 19:52:23 +0200

    Added tags html

--- src/components/github/GithubStartPage.jsx
+++ src/components/github/GithubStartPage.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import GithubAnalyzer from './Analyzer/GithubAnalyzer';
+import GithubComparer from './Comparer/GithubComparer';
 
 const GithubStartPage = () => ( 
-    <div>
-        Github chooser
+    <div className="container">
+        <ul id="tabs">
+            <li><a id="AnalyzerTab">Analyzer</a></li>
+            <li><a id="ComparerTab">Comparer</a></li>
+        </ul>
+        <div class="container" id="AnalyzerContainer">
+            <GithubAnalyzer/>
+        </div>
+        <div class="container" id="ComparerContainer">
+            <GithubComparer/>
+        </div>
     </div>
 );
  
