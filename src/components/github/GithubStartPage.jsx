cc09930bea940c28640b4a5e367af9058bd3ac8d
MartinKamenov <martinkamenov10@gmail.com>
2018-12-03 20:14:14 +0200

    Added styles to project

--- src/components/github/GithubStartPage.jsx
+++ src/components/github/GithubStartPage.jsx
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import GithubAnalyzer from './Analyzer/GithubAnalyzer';
 import GithubComparer from './Comparer/GithubComparer';
+import './github.css';
 
-const GithubStartPage = () => ( 
-    <div className="container">
-        <ul id="tabs">
-            <li><a id="AnalyzerTab">Analyzer</a></li>
-            <li><a id="ComparerTab">Comparer</a></li>
-        </ul>
-        <div class="container" id="AnalyzerContainer">
-            <GithubAnalyzer/>
-        </div>
-        <div class="container" id="ComparerContainer">
-            <GithubComparer/>
-        </div>
-    </div>
-);
+class GithubStartPage extends Component {
+    state = {
+        tab: 'Analyzer'
+    }
+
+    changeTab() {
+
+    }
+    render() {
+        return (
+            <div className="container center">
+                <ul id="tabs">
+                    <li className="tab"><a id="AnalyzerTab">Analyzer</a></li>
+                    <li className="tab"><a id="ComparerTab">Comparer</a></li>
+                </ul>
+                <div class="container" id="AnalyzerContainer">
+                    <GithubAnalyzer/>
+                </div>
+                <div class="container" id="ComparerContainer">
+                    <GithubComparer/>
+                </div>
+            </div>
+        );
+    }
+};
  
 export default GithubStartPage;
