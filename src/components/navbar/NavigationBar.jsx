c955d135b91c00a4cf44dc68468293d0d5693018
MartinKamenov <martinkamenov10@gmail.com>
2018-12-13 21:29:51 +0200

    Added style to header button

--- src/components/navbar/NavigationBar.jsx
+++ src/components/navbar/NavigationBar.jsx
@@ -5,8 +5,8 @@ import { Navbar, Nav, NavItem } from 'react-bootstrap';
 const NavigationBar = () => ( 
     <Navbar inverse collapseOnSelect>
         <Navbar.Header>
-            <Navbar.Brand>
-                <Link className="navbar-brand main_header main_header" to="/">Github Analyzator</Link>
+            <Navbar.Brand className="main-header-container">
+                <Link className="navbar-brand main-header" to="/">Github Analyzator</Link>
             </Navbar.Brand>
             <Navbar.Toggle>Menu</Navbar.Toggle>
         </Navbar.Header>
