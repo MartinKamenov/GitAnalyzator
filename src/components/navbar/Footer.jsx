47811dfd7d1c37812ee74faeffb7d08c08051bcf
MartinKamenov <martinkamenov10@gmail.com>
2018-12-18 19:17:58 +0200

    Added material icons for social network in footer

--- src/components/navbar/Footer.jsx
+++ src/components/navbar/Footer.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const FooterComponent = () => {
     return (
         <div className='footer-container'>
-            <div>Footer content</div>
+            <div>
+                    <div class="social-btns"><a class="btn facebook" href="#"><i class="fa fa-facebook"></i></a><a class="btn twitter" href="#"><i class="fa fa-twitter"></i></a><a class="btn google" href="#"><i class="fa fa-google"></i></a><a class="btn dribbble" href="#"><i class="fa fa-dribbble"></i></a><a class="btn skype" href="#"><i class="fa fa-skype"></i></a>
+                </div>
+            </div>
         </div>
     );
 }
