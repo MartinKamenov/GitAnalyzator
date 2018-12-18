e71bd188ecd9a2ae30dfcb88ebdf8d68c872824c
MartinKamenov <martinkamenov10@gmail.com>
2018-12-18 17:56:36 +0200

    Added home image to site.

--- src/components/home/HomePage.jsx
+++ src/components/home/HomePage.jsx
@@ -3,22 +3,27 @@ import { Link } from 'react-router-dom';
 import "../../index.css";
 
 const HomePage = () => {
-    return ( 
-        <div className="container center">
-            <h1>Github Analyzator</h1>
-            <div className="main-content">
-                <Link to="/analyze">Anlyze profile</Link> or <Link to="/compare">Compare profiles</Link>
-            </div>
-            <div className="sidebar">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg"
-                alt="Github"
-                className="github-img"></img>
-                <div>
-                    <p className="paragraph">
-                        Using Github we can analyze contributions for different users
-                    </p>
+    return (
+        <div className="home-wrapper">
+            <img className="home-image"
+                alt="home"
+                src="https://images.pexels.com/photos/1268477/pexels-photo-1268477.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"></img>
+            <div className="container center">
+                <div className="link-content">
+                    <Link to="/analyze">Anlyze profile</Link>
+                    <Link to="/compare">Compare profiles</Link>
                 </div>
-                <a href="https://github.com">Github</a>
+                <div className="sidebar">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg"
+                    alt="Github"
+                    className="github-img"></img>
+                    <div>
+                        <p className="paragraph">
+                            Using Github we can analyze contributions for different users
+                        </p>
+                    </div>
+                    <a href="https://github.com">Github</a>
+                </div>
             </div>
         </div>
     );
