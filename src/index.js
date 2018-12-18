0c0ab20a7b554c55195ca8aabdb6fc1a15006cef
MartinKamenov <martinkamenov10@gmail.com>
2018-12-18 21:23:48 +0200

    Remove material css for now.

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import configureStore from './store/configurationStore';
 import { Provider } from 'react-redux';
 import AppRouter from './AppRouter';
 import 'bootstrap/dist/css/bootstrap.css';
-import 'materialize-css/dist/css/materialize.min.css';
-import M from 'materialize-css/dist/js/materialize.min.js';
 
 const store = configureStore();
 
