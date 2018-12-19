c1e2d6e9c42a602e9205393b7ecfe8b4a2151962
MartinKamenov <martinkamenov10@gmail.com>
2018-12-19 18:26:35 +0200

    Fixed style social network buttons.

--- src/components/navbar/Footer.jsx
+++ src/components/navbar/Footer.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { FaFacebook, FaTwitter, FaGoogle, FaPhone } from 'react-icons/fa';
 
 const FooterComponent = () => {
     return (
         <div className='footer-container'>
+            <div className="container">
+                For contacts
+                <li>
+                    <ul>
+                        <span className="btn">
+                            <FaPhone/> 0886 99 08 41
+                        </span>
+                    </ul>
+                </li>
+            </div>
             <div>
-                    <div class="social-btns"><a class="btn facebook" href="#"><i class="fa fa-facebook"></i></a><a class="btn twitter" href="#"><i class="fa fa-twitter"></i></a><a class="btn google" href="#"><i class="fa fa-google"></i></a><a class="btn dribbble" href="#"><i class="fa fa-dribbble"></i></a><a class="btn skype" href="#"><i class="fa fa-skype"></i></a>
+                <div class="social-btns">
+                    <h3>Find us on social media</h3>
+                    <a class="btn facebook" href="https://facebook.com"><FaFacebook className="fa fa-facebook"/></a>
+                    <a class="btn twitter" href="https://twitter.com"><FaTwitter class="fa fa-twitter"/></a>
+                    <a class="btn google" href="https://google.com"><FaGoogle class="fa fa-twitter"/></a>
                 </div>
             </div>
         </div>
