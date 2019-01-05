3b46b232ddf31d037c3823c6058c184d2086013b
MartinKamenov <martinkamenov10@gmail.com>
2019-01-05 15:23:15 +0200

    Added graph to users.

--- src/components/github/Overview/ProfileDetailsComponent.jsx
+++ src/components/github/Overview/ProfileDetailsComponent.jsx
@@ -1,26 +1,49 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import ChartComponent from '../Chart/ChartComponent';
 
 const ProfileDetailsComponent = ({data}) => {
+    const contributionData = [];
+    data.dateContributionsNumbers.forEach((c, i) => contributionData.push([i, c]));
+    const dataArray = [{
+        title: 'Contribution graph', 
+        contributions: contributionData
+    }];
     return (
         <div className="details-container">
-            <div className="detail">
-                <span>All </span>
-                <div className="number-detail">{data.totalContributionsCount}</div>
+            <div className="details-numbers-container">
+                <div className="detail">
+                    <span>All </span>
+                    <div className="number-detail">{data.totalContributionsCount}</div>
+                </div>
+                <div className="detail">
+                    <span>Conclussive </span>
+                    <div className="number-detail">{data.conclussiveContributions}</div>
+                </div>
+                <div className="detail">
+                    <span>Max per day </span>
+                    <div className="number-detail">{data.maxContributionsForDay}</div>
+                </div>
+                <div className="detail">
+                    <span>Without </span>
+                    <div className="number-detail">{data.daysWithoutContributions}</div>
+                </div>
             </div>
-            <div className="detail">
-                <span>Conclussive </span>
-                <div className="number-detail">{data.conclussiveContributions}</div>
-            </div>
-            <div className="detail">
-                <span>Max per day </span>
-                <div className="number-detail">{data.maxContributionsForDay}</div>
-            </div>
-            <div className="detail">
-                <span>Without </span>
-                <div className="number-detail">{data.daysWithoutContributions}</div>
+            <div className="chart-wrapper">
+                <ChartComponent dataArray={dataArray}/>
             </div>
         </div>
     );
 }
+
+ProfileDetailsComponent.propTypes = {
+    data: PropTypes.shape({
+        totalContributionsCount: PropTypes.number.isRequired,
+        daysWithoutContributions: PropTypes.number.isRequired,
+        conclussiveContributions: PropTypes.number.isRequired,
+        maxContributionsForDay: PropTypes.number.isRequired,
+        dateContributionsNumbers: PropTypes.array.isRequired
+    }).isRequired
+};
  
 export default ProfileDetailsComponent;
