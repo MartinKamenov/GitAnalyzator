7822e923bd3343f03cdc02f2efebcb4920347952
MartinKamenov <martinkamenov10@gmail.com>
2019-01-05 13:11:22 +0200

    Added margins between profiles.

--- src/components/github/Overview/ProfileComponent.jsx
+++ src/components/github/Overview/ProfileComponent.jsx
@@ -5,12 +5,14 @@ import ProfileDetailsComponent from './ProfileDetailsComponent';
 const ProfileComponent = ({ profile }) => {
     return (
         <div className="profile-container col-md-4">
-            <h2>{profile.username}</h2>
-            <img
-            className="profile-image"
-            alt={profile.username}
-            src={profile.data.pictureUrl}></img>
-            <ProfileDetailsComponent data={profile.data}/>
+            <div className="profile">
+                <h2>{profile.username}</h2>
+                <img
+                className="profile-image"
+                alt={profile.username}
+                src={profile.data.pictureUrl}></img>
+                <ProfileDetailsComponent data={profile.data}/>
+            </div>
         </div>
     );
 }
