9da7ba51c4a92e650c280a851c638609bd3a8b6f
MartinKamenov <martinkamenov10@gmail.com>
2019-01-07 22:42:00 +0200

    Added paging css.

--- src/components/github/Common/paging/PagingComponent.jsx
+++ src/components/github/Common/paging/PagingComponent.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './paging.css';
+import PagingDetailsComponent from './PagingDetailsComponent';
 
 const PagingComponent = ({ pages }) => (
-    <div></div>
+    <div>
+        {
+            pages.map((page, i) => {
+                return <PagingDetailsComponent page={page} key={i}/>
+            })
+        }
+    </div>
 );
 
 PagingComponent.propTypes = {
--- src/components/github/Common/paging/PagingDetailsComponent.jsx
+++ src/components/github/Common/paging/PagingDetailsComponent.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const PagingDetailsComponent = ({ page }) => {
-    return (  );
-};
+const PagingDetailsComponent = ({ page }) => (
+    <span className='page-container'>{ page }</span>
+);
 
 PagingDetailsComponent.propTypes = {
     page: PropTypes.number.isRequired
