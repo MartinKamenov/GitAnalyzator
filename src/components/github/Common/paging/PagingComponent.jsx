41477f9a64136f8aed051aa87afe5e045b53f97f
MartinKamenov <martinkamenov10@gmail.com>
2019-01-07 21:36:20 +0200

    Added paging components.

---
+++ src/components/github/Common/paging/PagingComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './paging.css';
+
+const PagingComponent = ({ pages }) => (
+    <div></div>
+);
+
+PagingComponent.propTypes = {
+    pages: PropTypes.array.isRequired
+};
+ 
+export default PagingComponent;
---
+++ src/components/github/Common/paging/PagingDetailsComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const PagingDetailsComponent = ({ page }) => {
+    return (  );
+};
+
+PagingDetailsComponent.propTypes = {
+    page: PropTypes.number.isRequired
+};
+ 
+export default PagingDetailsComponent;
