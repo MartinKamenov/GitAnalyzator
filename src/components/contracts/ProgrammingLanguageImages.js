f25aba5de2a60b8f19f0936e52f77ff5b8dc3e0b
MartinKamenov <martinkamenov10@gmail.com>
2019-01-22 18:37:33 +0200

    Added more language icons

--- src/components/contracts/ProgrammingLanguageImages.js
+++ src/components/contracts/ProgrammingLanguageImages.js
@@ -1,7 +1,14 @@
 const ProgrammingLanguageImages = {
     'C#': '/languages/cSharp.png',
     'JavaScript': '/languages/js.png',
-    'Java': '/languages/java.png'
+    'Java': '/languages/java.png',
+    'PHP': '/languages/php.png',
+    'Python': '/languages/python.png',
+    'HTML': '/languages/html.png',
+    'C': '/languages/c.png',
+    'C++': '/languages/cPlusPlus.png',
+    'CSS': '/languages/css.png',
+    'undefined': '/languages/undefined.png'
 };
 
 export default ProgrammingLanguageImages;
