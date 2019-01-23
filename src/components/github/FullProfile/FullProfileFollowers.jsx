516c3c2732602d10b8d66df178f55b78b677baee
MartinKamenov <martinkamenov10@gmail.com>
2019-01-23 20:05:30 +0200

    Added formatting of followers data

--- src/components/github/FullProfile/FullProfileFollowers.jsx
+++ src/components/github/FullProfile/FullProfileFollowers.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const FullProfileFollowers = ({followers}) => (
-    <div>
+    <div className='row'>
         {
             followers.map((follower) => 
                 <a
-                    className='follower-container'
+                    className='follower-container col-md-2'
                     href={'/profile/' + follower}>
                     {follower}
                 </a>
