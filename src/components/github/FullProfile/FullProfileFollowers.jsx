d91918c0b987c55026b50beb9e161bc2515498dd
MartinKamenov <martinkamenov10@gmail.com>
2019-01-25 19:29:31 +0200

    Every follower should be in lower case.

--- src/components/github/FullProfile/FullProfileFollowers.jsx
+++ src/components/github/FullProfile/FullProfileFollowers.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 
-const FullProfileFollowers = ({followers}) => (
-    <div className='row'>
-        {
-            followers.map((follower) => 
-                <a
-                    className='follower-container col-md-2'
-                    href={'/profile/' + follower}>
-                    {follower}
-                </a>
-            )
-        }
-    </div>
-);
+const FullProfileFollowers = ({followers}) => {
+    followers = followers.map(f => f.toLowerCase());
+    return (
+        <div className='row'>
+            {
+                followers.map((follower) => 
+                    <a
+                        className='follower-container col-md-2'
+                        href={'/profile/' + follower}>
+                        {follower}
+                    </a>
+                )
+            }
+        </div>
+)};
  
 export default FullProfileFollowers;
