9ae8c2f220f73b73f613c8043507a5958fdaed0e
MartinKamenov <martinkamenov10@gmail.com>
2019-03-12 21:49:35 +0200

    Made navigation bar sticky.

--- src/components/navbar/NavigationBar.jsx
+++ src/components/navbar/NavigationBar.jsx
@@ -2,37 +2,39 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
 
-const NavigationBar = () => ( 
-    <Navbar inverse collapseOnSelect className="main-navbar">
-        <Navbar.Header>
-            <Navbar.Brand className="main-header-container">
-                <Link className="navbar-brand main-header" to="/">Github Analyzator</Link>
-            </Navbar.Brand>
-            <Navbar.Toggle>Menu</Navbar.Toggle>
-        </Navbar.Header>
-        <Navbar.Collapse>
-            <Nav>
-                <NavItem componentClass='span' className="nav-link-container">
-                    <Link className="nav-link navbar_element" to="/">Home</Link>
-                </NavItem>
-            </Nav>
-            <Nav>
-                <NavItem componentClass='span' className="nav-link-container">
-                    <Link className="nav-link navbar_element" to="/overview">Overview</Link>
-                </NavItem>
-            </Nav>
-            <Nav>
-                <NavItem componentClass='span' className="nav-link-container">
-                    <Link className="nav-link navbar_element" to="/analyze">Analyze</Link>
-                </NavItem>
-            </Nav>
-            <Nav>
-                <NavItem componentClass='span' className="nav-link-container">
-                    <Link className="nav-link navbar_element" to="/compare">Compare</Link>
-                </NavItem>
-            </Nav>
-        </Navbar.Collapse>
-    </Navbar>
+const NavigationBar = () => (
+    <React.Fragment>
+        <Navbar inverse collapseOnSelect className="main-navbar sticky">
+            <Navbar.Header>
+                <Navbar.Brand className="main-header-container">
+                    <Link className="navbar-brand main-header" to="/">Github Analyzator</Link>
+                </Navbar.Brand>
+                <Navbar.Toggle>Menu</Navbar.Toggle>
+            </Navbar.Header>
+            <Navbar.Collapse>
+                <Nav>
+                    <NavItem componentClass='span' className="nav-link-container">
+                        <Link className="nav-link navbar_element" to="/">Home</Link>
+                    </NavItem>
+                </Nav>
+                <Nav>
+                    <NavItem componentClass='span' className="nav-link-container">
+                        <Link className="nav-link navbar_element" to="/overview">Overview</Link>
+                    </NavItem>
+                </Nav>
+                <Nav>
+                    <NavItem componentClass='span' className="nav-link-container">
+                        <Link className="nav-link navbar_element" to="/analyze">Analyze</Link>
+                    </NavItem>
+                </Nav>
+                <Nav>
+                    <NavItem componentClass='span' className="nav-link-container">
+                        <Link className="nav-link navbar_element" to="/compare">Compare</Link>
+                    </NavItem>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    </React.Fragment>
 );
  
 export default NavigationBar;
