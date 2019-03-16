9e153ca8dcc830e5ffa156099399536a9ba600ec
MartinKamenov <martinkamenov10@gmail.com>
2019-03-16 18:20:08 +0200

    Fixed styling of link of contributors in repository.

--- src/components/github/Repository/RepositoryContributorsListComponent.jsx
+++ src/components/github/Repository/RepositoryContributorsListComponent.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const RepositoryContributorsListComponent = ({contributors}) => (
     <div>{contributors.map((contributor, i) => {
-        return <div key={i}>
-            <h4>{contributor.name}</h4>
-            <img className="profile-image-small"
-                alt={contributor.name}
-                src={contributor.imageSrc}/>
-        </div>
+        return <Link to={'/profile/' + contributor.name}
+            key={i} className='repository-contributor-link'>
+            <div className='repository-contributor-wrapper'>
+                <h4>{contributor.name}</h4>
+                <img className="profile-image-small"
+                    alt={contributor.name}
+                    src={contributor.imageSrc}/>
+                <div>
+                    <div>Commits: {contributor.contributions}</div>
+                    <div>Additions: {contributor.additions}</div>
+                    <div>Deletions: {contributor.deletions}</div>
+                </div>
+            </div>
+        </Link>
     })}</div>
 );
 
